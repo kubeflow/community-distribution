aed22f07be7eeae4b847001a3a1229f7b0d6062b
danish9039 <danishsiddiqui040@gmail.com>
2026-05-27 13:15:57 +0530

    test: enable katib security context config
    Signed-off-by: danish9039 <danishsiddiqui040@gmail.com>

--- tests/katib_install.sh
+++ tests/katib_install.sh
@@ -4,21 +4,23 @@ set -euxo pipefail
 KATIB_CI_OVERLAY=$(mktemp -d .katib-ci-overlay.XXXXXX)
 trap 'rm -rf "$KATIB_CI_OVERLAY"' EXIT
 
+awk '
+  { print }
+  $0 == "    webhookPort: 8443" { print "    injectSecurityContext: true" }
+' applications/katib/upstream/installs/katib-cert-manager/katib-config.yaml > "$KATIB_CI_OVERLAY/katib-config.yaml"
+
 cat > "$KATIB_CI_OVERLAY/kustomization.yaml" <<'EOF'
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
 - ../applications/katib/upstream/installs/katib-with-kubeflow
-patches:
-- target:
-    group: apps
-    version: v1
-    kind: Deployment
-    name: katib-controller
-  patch: |-
-    - op: add
-      path: /spec/template/spec/containers/0/args/-
-      value: --inject-security-context=true
+generatorOptions:
+  disableNameSuffixHash: true
+configMapGenerator:
+- name: katib-config
+  behavior: replace
+  files:
+  - katib-config.yaml
 EOF
 
 kustomize build "$KATIB_CI_OVERLAY" | kubectl apply -f -
