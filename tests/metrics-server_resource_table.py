56031476df2ec61558f08cb16c235d40eff4a252
juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
2026-07-05 14:47:47 +0200

    .
    Signed-off-by: juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>

--- tests/metrics-server_resource_table.py
+++ tests/metrics-server_resource_table.py
@@ -35,7 +35,10 @@ COMPONENT_RULES = {
     # },
     "Volumes Web Application": {"keywords": ["volumes-web-app"]},
     "Katib": {"keywords": ["katib"]},
-    "KServe": {"keywords": ["kserve", "predictor"], "exclude": ["models-web-application", "kserve-ui"]},
+    "KServe": {
+        "keywords": ["kserve", "predictor"],
+        "exclude": ["models-web-application", "kserve-ui"],
+    },
     "KServe UI": {"keywords": ["models-web-application", "kserve-ui"]},
     "Kubeflow Pipelines": {
         "keywords": [
